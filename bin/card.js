d0201e784b54880353d9250ff3aa5bc42c8b7a67
Michael Pumo <hello@michaelpumo.com>
2018-12-20 17:00:30 +0000

    Update with my credentials.

--- bin/card.js
+++ bin/card.js
@@ -14,14 +14,14 @@ const options = {
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('Tierney Cyren /'),
-  handle: chalk.cyan('bitandbang'),
-  work: chalk.white('Senior Cloud Developer Advocate at Microsoft'),
-  twitter: chalk.cyan('https://twitter.com/bitandbang'),
-  github: chalk.cyan('https://github.com/bnb'),
-  linkedin: chalk.cyan('https://linkedin.com/in/bitandbang'),
-  web: chalk.cyan('https://bnb.im'),
-  npx: chalk.white('npx bitandbang'),
+  name: chalk.white('Michael Pumo /'),
+  handle: chalk.cyan('michaelpumo'),
+  work: chalk.white('Frontend Web Developer'),
+  twitter: chalk.cyan('https://twitter.com/michaelpumo'),
+  github: chalk.cyan('https://github.com/michaelpumo'),
+  linkedin: chalk.cyan('https://linkedin.com/in/michaelpumo'),
+  web: chalk.cyan('https://michaelpumo.com'),
+  npx: chalk.white('npx michaelpumo'),
   labelWork: chalk.white.bold('      Work:'),
   labelTwitter: chalk.white.bold('   Twitter:'),
   labelGitHub: chalk.white.bold('    GitHub:'),
